a6d130b6a0960723e7939f89572d534c4108f28b
matomugo60 <matomugo60@gmail.com>
2022-12-08 14:47:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
+
+  for (const number of array) {
+    const complement = target - number;
+
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
 
+// 0(n) runtime
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
+
+// 0(1) runtime
+function findSock(object) {
+  if (object.sock) return "sock";
+}
+
 /* 
   Add your pseudocode here
+  hasTargetSum([1,2,3,4], 6)
+  seenNumbers = {
+    1: true,
+    2: true,
+    3: true
+  }
+  create an object to keep track of numbers we've already seen
+  iterate through each number in the array
+    for the current num, identify a complement that adds to the target (comp = target - num)
+    check if any key on our object is the complement
+      if so, return true
+      otherwise, add that number to the object
+  if I reach the end of the array, return false
 */
 
 /*
@@ -29,6 +63,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 7, 4], 6));
 }
 
 module.exports = hasTargetSum;
